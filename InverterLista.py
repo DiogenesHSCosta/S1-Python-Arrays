505f254ced6efbb00d6a68281f5c2e48e8fd9c5e
DiogenesHSCosta <Diogenes.henriquecosta@outlook.com>
2024-05-14 22:37:36 -0300

    inverter lista

---
+++ InverterLista.py
@@ -0,0 +1,15 @@
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+def inverterLista(lista):
+    tamanho = len(lista) -1
+
+                  #pega numero inteiro da divisão
+    for i in range(len(lista)//2):
+        aux = lista[i]
+        lista[i] = lista[tamanho- i]
+        lista[tamanho - i] = aux
+
+    return lista
+
+
+print(inverterLista(numeros))
